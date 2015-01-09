3965974e7a5964c2af67459dc6fc63d442d36e81
Rui Carmo <rui.carmo@gmail.com>
2015-01-09 15:57:55 +0000

    updated dependencies

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "Clojure library for the Hazelcast p2p cluster"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.runa/clj-kryo "1.4.1"]
-                 [com.hazelcast/hazelcast "3.3"]
+                 [com.hazelcast/hazelcast "3.4"]
                  ]
-  :profiles {:provided {:dependencies [[org.clojure/tools.logging "0.2.6"]]}}
+  :profiles {:provided {:dependencies [[org.clojure/tools.logging "0.3.1"]]}}
   :global-vars {*warn-on-reflection* true}
   :min-lein-version "2.0.0" ;provided deps
   :java-source-paths ["src-java"]
