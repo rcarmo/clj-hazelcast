36636ce121733637da6f23e0d4775c21e65e0492
Rui Carmo <rui.carmo@gmail.com>
2015-07-08 18:57:18 +0100

    Updated deps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.clojars.runa/clj-hazelcast "1.2.1"
   :description "Clojure library for the Hazelcast p2p cluster"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojars.runa/clj-kryo "1.5.0"]
-                 [com.hazelcast/hazelcast "3.4"]]
+                 [com.hazelcast/hazelcast "3.5"]]
   :profiles {:provided {:dependencies [[org.clojure/tools.logging "0.2.6"]]}}
   :global-vars {*warn-on-reflection* true}
   :min-lein-version "2.0.0" ;provided deps
